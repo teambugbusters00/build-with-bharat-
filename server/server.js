c38d539bbf9049f757b666b3e89786ffe3b01694
Aayush Laddha <aayushladdha001@gmail.com>
2025-11-26 00:35:21 +0530

    Add PORT to .env

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import reportRoutes from "./routes/reportRoutes.js";
 
 dotenv.config();
 const app = express();
-PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
