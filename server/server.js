6c508634bbade3536c76875a9f5c971a62d1b7fe
Aayush Laddha <aayushladdha001@gmail.com>
2025-11-26 00:22:05 +0530

    Add PORT to .env

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import reportRoutes from "./routes/reportRoutes.js";
 
 dotenv.config();
 const app = express();
+PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,4 +17,4 @@ mongoose.connect(process.env.REPORT_MONGODB_URI)
 
 app.use("/api/report", reportRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log("Server running on port 5000"));
